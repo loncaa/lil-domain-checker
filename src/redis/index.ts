973b6fb6d7ea259617e008a01cbfec13d7b5b2fc
Antonio <antonio.loncar@kickstage.com>
2021-10-07 15:52:00 +0200

    added logging

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -2,10 +2,20 @@ import * as Redis from 'ioredis';
 import * as RedisLock from 'redis-lock';
 import * as Utils from 'util';
 
+import logger from '../loggers/winston';
+
 const REDISTOGO_URL = process.env.REDISTOGO_URL || `localhost:6379`;
 
 const redisClient = new Redis(REDISTOGO_URL);
 const redisLock = Utils.promisify(new RedisLock(redisClient));
+
+redisClient.on('connect', () => {
+  logger.info('Successfully connected to Redis!');
+});
+
+redisClient.on('error', (error) => {
+  logger.error(`Error occurred while connecting to Redis. ${error}`);
+});
 
 export {
   redisClient,
